25595265515c968cf8a62bcedfbc8265b33e42da
Krzysztof <vescus@gmail.com>
2025-12-10 21:47:50 +0100

    extended validation and tests

--- src/main/scala/Product.scala
+++ src/main/scala/Product.scala
@@ -1,9 +1,11 @@
 package com.pleasure
 
 import zio.json.{DeriveJsonDecoder, DeriveJsonEncoder, JsonDecoder, JsonEncoder}
+import zio.prelude.Validation
 
 enum Category:
-  case Dildos, Lingerie, UsedDildos, UsedLingerie, BDSM, Furniture, Accessories, UsedAccessories, Lubricants
+  case Dildos, Lingerie, UsedDildos, UsedLingerie, BDSM, Furniture, Accessories,
+    UsedAccessories, Lubricants
 
 enum OfferType:
   case ForSale(price: BigDecimal)
@@ -11,32 +13,31 @@ enum OfferType:
   case ForBoth(dailyRate: BigDecimal, price: BigDecimal, deposit: BigDecimal)
 
   def canBeSold: Boolean = this match
-    case ForSale(_) => true
-    case ForRental(_, _) => false
+    case ForSale(_)       => true
+    case ForRental(_, _)  => false
     case ForBoth(_, _, _) => true
 
   def canBeRented: Boolean = this match
-    case ForSale(_) => false
-    case ForRental(_, _) => true
+    case ForSale(_)       => false
+    case ForRental(_, _)  => true
     case ForBoth(_, _, _) => true
 
   def salePrice: Option[BigDecimal] = this match
-    case ForSale(price) => Some(price)
-    case ForRental(_, _) => None
+    case ForSale(price)           => Some(price)
+    case ForRental(_, _)          => None
     case ForBoth(_, salePrice, _) => Some(salePrice)
 
   def rentalInfo: Option[(BigDecimal, BigDecimal)] = this match
-    case ForSale(price) => None
-    case ForRental(dailyRate, deposit) => Some((dailyRate, deposit))
+    case ForSale(price)                     => None
+    case ForRental(dailyRate, deposit)      => Some((dailyRate, deposit))
     case ForBoth(dailyRate, price, deposit) => Some((dailyRate, deposit))
 
-
 final case class Product(
-                          id: ProductId,
-                          name: String,
-                          category: Category,
-                          offerType: OfferType
-                        ):
+    id: ProductId,
+    name: String,
+    category: Category,
+    offerType: OfferType
+):
   def canBeSold: Boolean = offerType.canBeSold
 
   def canBeRented: Boolean = offerType.canBeRented
@@ -46,32 +47,60 @@ final case class Product(
   def rentalInfo: Option[(BigDecimal, BigDecimal)] = offerType.rentalInfo
 
 object Product:
-  def create(name: String,
-             category: Category,
-             offerType: OfferType
-            ): Either[String, Product] =
-              for 
-                _ <- Either.cond(name.trim.nonEmpty, (), "Name cannot be empty")
-                _ <- validateOfferType(offerType)
-              yield new Product(ProductId.generate, name.trim, category, offerType)
+  def create(
+      name: String,
+      category: Category,
+      offerType: OfferType
+  ): Either[String, Product] =
+    val validation = Validation.validateWith(
+      validateName(name),
+      validateOfferType(offerType)
+    )((validName, _) =>
+      new Product(
+        id = ProductId.generate,
+        name = validName,
+        category = category,
+        offerType = offerType
+      )
+    )
 
-  def validateOfferType(offerType: OfferType): Either[String, Unit] =
+    validation.toEither.left.map(_.mkString(", "))
+
+  private def validateName(name: String): Validation[String, String] =
+    Validation.fromPredicateWith("Name cannot be empty")(name.trim)(_.nonEmpty)
+
+  def validateOfferType(offerType: OfferType): Validation[String, Unit] =
     offerType match
       case OfferType.ForSale(price) =>
-         Either.cond(price > 0, (), "Sale price must be greater than zero")
+        validatePositive(price, "Sale price")
       case OfferType.ForRental(dailyRate, deposit) =>
-        for
-          _ <- Either.cond(dailyRate > 0, (), "Daily rate must be greater than zero")
-          _ <- Either.cond(deposit > 0, (), "Deposint must be non-negative")
-          _ <- Either.cond(deposit >= dailyRate, (), "Deposit must be at least equal to daily rate")
-        yield ()
+        Validation.validateWith(
+          validatePositive(dailyRate, "Daily rate"),
+          validatePositive(deposit, "Deposit"),
+          validateDepositVsRate(deposit, dailyRate)
+        )((_, _, _) => ())
       case OfferType.ForBoth(dailyRate, price, deposit) =>
-        for
-          _ <- Either.cond(price > 0, (), "Price must be greater than zero")
-          _ <- Either.cond(dailyRate > 0, (), "Daily rate must be greater than zero")
-          _ <- Either.cond(deposit > 0, (), "Deposint must be non-negative")
-          _ <- Either.cond(deposit >= dailyRate, (), "Deposit must be at least equal to daily rate")
-        yield ()
+        Validation.validateWith(
+          validatePositive(price, "Price"),
+          validatePositive(dailyRate, "Daily rate"),
+          validatePositive(deposit, "Deposit"),
+          validateDepositVsRate(deposit, dailyRate)
+        )((_, _, _, _) => ())
+
+  private def validatePositive(
+      value: BigDecimal,
+      fieldName: String
+  ): Validation[String, Unit] =
+    Validation.fromPredicateWith(s"$fieldName must be greater than zero")(())(
+      _ => value > 0
+    )
+  private def validateDepositVsRate(
+      deposit: BigDecimal,
+      dailyRate: BigDecimal
+  ): Validation[String, Unit] =
+    Validation.fromPredicateWith(
+      "Deposit must be at least equal to daily rate"
+    )(())(_ => deposit >= dailyRate)
 
   given JsonEncoder[Product] = DeriveJsonEncoder.gen[Product]
   given JsonDecoder[Product] = DeriveJsonDecoder.gen[Product]
